2c4e9259dc8e0b58b46d7779b415393bd0382371
Donat Csikos <donat@gradle.com>
2021-10-14 13:47:00 +0200

    Adjust test plan instructions

--- generateTestPlan.groovy
+++ generateTestPlan.groovy
@@ -1,6 +1,6 @@
 /**
  * Generates instructions on how to execute manual IDE smoke tests.
- * 
+ *
  * The script iterates through all (Java, Groovy, Kotlin and Gradle) source files, grabs the comments starting with
  * `// TODO (scenario)` and outputs the list of scenarios along with instructions on how to perform the
  * tests.
@@ -30,11 +30,38 @@ workingDir.eachFile {File sampleDir ->
 renderTestPlan(allScenarios)
 
 void renderTestPlan(Map<String, List<String>> allScenarios) {
-    println "# Test Plan"
+    println """
+        # Test Plan
+
+        ## Prerequisites
+
+        - Install required IDEs
+        - Install IntelliJ IDEA, EAP and latest stable release
+        - Clone this repository
+        - Update Gradle wrappers to the latest snapshot version
+        - Clean projects
+          - `git clean -fdx`
+
+        ## Verify scenarios
+
+        The sample projects are located in the repository root and their name all starts with a number. For each project, execute the following steps:
+        - Start the EAP version of IDEA
+        - Import the sample project into IDEA and wait for the project sync to finish
+        - Use text search to locate the scenario (listed below) in the build
+        - Verify the functionality in the IDE
+        - Check off the verified scenario in the list below.
+
+        In case of a failure
+        - Verify the same scenario in the latest released Idea version
+        - Open an issue at https://youtrack.jetbrains.com
+        - Add a `// Known issue: https://link-to-new-issue` comment to corresponding file in the sample project
+        - add a X (red cross emoji) to the list below
+    """.stripIndent(8).strip()
+
 
     allScenarios.each {project, scenarios ->
         println ""
-        println "## $project"
+        println "## Scenarios in `$project`"
         println ""
         scenarios.each { scenario ->
             println "- [ ] $scenario"
