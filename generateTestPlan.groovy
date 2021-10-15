fcf4c676ebecbc3c68ff850311da42e242af466e
Donat Csikos <donat@gradle.com>
2021-10-15 13:14:31 +0200

    Improve GitHub issue template

--- generateTestPlan.groovy
+++ generateTestPlan.groovy
@@ -30,58 +30,68 @@ workingDir.eachFile {File sampleDir ->
 renderTestPlan(allScenarios)
 
 void renderTestPlan(Map<String, List<String>> allScenarios) {
-    // TODO add year/week parameters
-    // TODO add assignees
+    Calendar calendar = Calendar.getInstance()
+    def year = calendar.get(Calendar.YEAR).getClass()
+    def weekOfYear = calendar.get(Calendar.WEEK_OF_YEAR)
     println """
         ---
-        name: Test Plan 2021.41
-        about: Test plan for manual IDE smoke tests
-        title: Test Plan 2021.41
+        name: Test Iteration ${year}.${weekOfYear}
+        about: Instructions to execute manual IDE smoke tests
+        title: Test Iteration ${year}.${weekOfYear}
         labels: test-iteration
         ---
 
         # Test Plan
 
-        ## Prerequisites
+        ## 1. Set up local test environment
 
-        - Install required IDEs
+        - Assing this ticket to yourself and move it to the the _In Progress_ column.
         - Install IntelliJ IDEA, EAP and latest stable release
-        - Clone this repository
-        - Update Gradle wrappers to the latest snapshot version
-        - Clean projects
-          - `git clean -fdx`
+          - Tip: you can use the [Toolbox App](https://www.jetbrains.com/toolbox-app/) to manage multiple installations
+        - Clone [this](https://github.com/gradle/ide-smoke-tests) repository
+        - Update Gradle wrappers in all sample projects to the latest snapshot
+        - Run `git clean -fdx` to remove build artifacts from all sample projects
 
-        ## Document components used in the test plan:
+        ## 2. Document components:
         
         - [ ] Operating System:
         - [ ] Gradle version: 
         - [ ] IntelliJ Idea EAP version (with build number):
         - [ ] IntelliJ Idea Stable version (with build number): 
 
-        ## Verify scenarios
+        ## 3. Verify scenarios
 
-        The sample projects are located in the repository root and their name all starts with a number. For each project, execute the following steps:
-        - Start the EAP version of IDEA
+        The sample projects are located in the numbered folders in the repository root. For each project, follow the steps below:
+
+        - Launch IDEA EAP
         - Import the sample project into IDEA and wait for the project sync to finish
-        - Use text search to locate the scenario (listed below) in the build
-        - Verify the functionality in the IDE
-        - Check off the verified scenario in the list below.
+        - Use text search to locate the scenario (listed below) in the build and follow the instructions
+        - Check off the verified scenario in the list below
 
         In case of a failure
-        - Verify the same scenario in the latest released Idea version
+        - Verify the same scenario in the latest released IDEA version
         - Open an issue at https://youtrack.jetbrains.com
-        - Add a `// Known issue: https://link-to-new-issue` comment to corresponding file in the sample project
-        - add a X (red cross emoji) to the list below
+        - Link the issue in the sample project:
+          -  Add the following comment to the failing line `// Known issue: https://youtrack.jetbrains.com/issue/IDEA-123456`
+        - add a X (red cross emoji) to the list below and link the created issue there as well
     """.stripIndent(8).strip()
 
     allScenarios.each {project, scenarios ->
         println ""
-        println "## Scenarios in `$project`"
+        println "### Scenarios in `$project`"
         println ""
         scenarios.each { scenario ->
             println "- [ ] $scenario"
         }
     }
+    println """
+        ## 4. Finalize
+        
+        - Close this issue
+        - Consider improving the test plan
+          - If you find that some scenarios are incorrect, redundant, or missing, please provide a PR and ask the @bt-support-team for a review
+          - You can update the issue template here: https://github.com/gradle/ide-smoke-tests/blob/main/generateTestPlan.groovy
+    """.stripIndent(8).stripTrailing()
 }
 
 boolean isSourceFile(File file) {
