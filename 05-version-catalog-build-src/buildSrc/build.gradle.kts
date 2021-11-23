2db04f2f70f98e19d3d322dd2ae22880161596cd
Donat Csikos <donat@gradle.com>
2021-11-23 11:01:25 +0100

    Add known issue

--- 05-version-catalog-build-src/buildSrc/build.gradle.kts
+++ 05-version-catalog-build-src/buildSrc/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
     //   - Wait for the import to finish
     //   - Check whether the generated dependency accessor is detected by the IDE
     implementation(libs.commonsLang3) // Known issue: https://youtrack.jetbrains.com/issue/KTIJ-19370
+                                      // Known issue: https://youtrack.jetbrains.com/issue/IDEA-283143
 }
