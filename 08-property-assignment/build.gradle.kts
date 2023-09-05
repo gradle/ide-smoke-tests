4c5ec263c1bceaa78886bf77f50d5235230d6d69
Bálint Hegyi <bhegyi@gradle.com>
2023-09-05 14:03:02 +0100

    Add set property assignment

--- 08-property-assignment/build.gradle.kts
+++ 08-property-assignment/build.gradle.kts
@@ -13,6 +13,10 @@ open class PropertyTestTask : DefaultTask() {
     val mapProperty = project
         .objects
         .mapProperty<Any, Any>()
+
+    val setProperty = project
+        .objects
+        .setProperty<Any>()
 }
 
 tasks {
@@ -31,6 +35,12 @@ tasks {
         //   - the statement below should not show any syntax errors
         //   - navigating to the assignment operator should navigate to the `PropertyExtensionsKt#assign` method
         fileSystemLocationProperty = project.layout.buildDirectory.dir("directory")
+
+        // TODO (scenario) Kotlin set property assignment should be supported
+        //   Instructions:
+        //   - the statement below should not show any syntax errors
+        //   - navigating to the assignment operator should navigate to the `PropertyExtensionsKt#assign` method
+        setProperty = setOf("value")
 
         // TODO (scenario) Kotlin map property assignment should be supported
         //   Instructions:
