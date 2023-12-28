3ed6a1ae4ebb53e2eb2739ba6270cbe3234f05d3
Reinhold Degenfellner <rdegenfellner@gradle.com>
2023-12-28 10:41:05 +0100

    upgrade gradle

--- upgrade_gradle.sh
+++ upgrade_gradle.sh
@@ -1,2 +1,6 @@
+
 #!/usr/bin/env bash
+
+#for sample in $(ls | grep -e '^[0-9][0-9].*'); do cd $sample; ../../../../localDist/bin/gradle  wrapper --gradle-version nightly; cd -; done
+
 for sample in $(ls | grep -e '^[0-9][0-9].*'); do cd $sample; ./gradlew wrapper --gradle-version nightly; cd -; done
