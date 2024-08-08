6a4bc8ac9f002f31c85e1990a69da08ae1f9f518
Reinhold Degenfellner <rdegenfellner@gradle.com>
2024-08-08 12:41:37 +0200

    known issue has been fixed

--- 01-build-src/settings.gradle.kts
+++ 01-build-src/settings.gradle.kts
@@ -4,10 +4,9 @@
 //   - Open the `Build` tool window
 //   - Check the output for warning or error messages
 //   - Check the output for deprecation warnings
-// Known Issue: https://youtrack.jetbrains.com/issue/IDEA-328159/kotlin-plugin-is-call-deprecated-projtect.convention-method
 rootProject.name = "ide-smoke-test-build-src"
 
 // TODO (scenario) Code completion available for settings script in project root directory
 //   Instructions:
-//   - Move the cursor to the last line in this file (14)
+//   - Move the cursor to the last line in this file (13)
 //   - Press ctrl+space (Basic code completion) and check if the content assist shows the expected options
