263aaf9b004c69aad9b74fa5bdf1f004f753387a
Reinhold Degenfellner <rdegenfellner@gradle.com>
2024-08-08 14:10:13 +0200

    Remove now fixed known issue mention

--- 05-version-catalog-build-src/buildSrc/build.gradle.kts
+++ 05-version-catalog-build-src/buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    // TODO (scenario) Project import finishes without warning [Known Issue](https://github.com/melix/jmh-gradle-plugin/issues/235)
+    // TODO (scenario) Project import finishes without warning
     //   Instructions:
     //   - Wait for the import to finish
     //   - Check whether the generated dependency accessor is detected by the IDE via hovering over the commonslang3 text
